1c9a128ac093337316c05491f70d765ff037a1e1
Diego Rabatone Oliveira <diraol@diraol.eng.br>
2025-06-17 10:48:46 -0300

    Avoid duplicates on config files

--- scripts/install-nix-ubuntu.sh
+++ scripts/install-nix-ubuntu.sh
@@ -3,33 +3,56 @@
 set -ve
 
 sudo apt update
-sudo apt install -y vim git curl build-essential dkms linux-headers-$(uname -r) bspwm libglfw3-dev libgles2-mesa-dev libgl1-mesa-dev xorg-dev
+sudo apt install -y vim git curl build-essential dkms linux-headers-"$(uname -r)" bspwm libglfw3-dev libgles2-mesa-dev libgl1-mesa-dev xorg-dev
 #git clone https://github.com/ericdallo/dotfiles.git ~/.dotfiles
 
 sudo mkdir -p /nix
-sudo chown "$USERNAME" /nix
+sudo chown "${USERNAME:-diraol}" /nix
 sh <(curl -L https://nixos.org/nix/install) --no-daemon
-source "/home/$USERNAME/.nix-profile/etc/profile.d/nix.sh"
+source "/home/${USERNAME:-diraol}/.nix-profile/etc/profile.d/nix.sh"
 
-echo "export NIX_PATH=$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels${NIX_PATH:+:$NIX_PATH}" >> ~/.bashrc
-echo "export NIXPKGS_ALLOW_UNFREE=1" >> ~/.bashrc
+NEW_PATH="${HOME}/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels${NIX_PATH:+:$NIX_PATH}"
+
+if ! grep -q "NIX_PATH=${NIX_PATH}" ~/.bashrc ; then
+	echo "export NIX_PATH=${NEW_PATH}" >> ~/.bashrc
+	echo "export NIXPKGS_ALLOW_UNFREE=1" >> ~/.bashrc
+fi
 
 nix-env -iA nixpkgs.home-manager
 
 mkdir -p ~/.config/nix
-echo "experimental-features = nix-command flakes" > ~/.config/nix/nix.conf
+EXP_FEAT="experimental-features = nix-command flakes"
+if ! grep -q "${EXP_FEAT}" ~/.config/nix/nix.conf; then
+	echo "" > ~/.config/nix/nix.conf
+fi
 
 cd ~/.dotfiles/nix
 home-manager switch --flake .
 
-echo "export XDG_DATA_DIRS=$HOME/.nix-profile/share:$HOME/.share:\"${XDG_DATA_DIRS:-/usr/local/share/:/usr/share/}\"" >> ~/.profile
-echo "export XDG_DATA_DIRS=$HOME/.nix-profile/share:$HOME/.share:\"${XDG_DATA_DIRS:-/usr/local/share/:/usr/share/}\"" >> ~/.zprofile
+NEW_XDG_DATA_DIRS="${HOME}/.nix-profile/share:${HOME}/.share:\"${XDG_DATA_DIRS:-/usr/local/share/:/usr/share/}\""
+if ! grep -q "${NEW_XDG_DATA_DIRS}" ~/.profile ; then
+	echo "export XDG_DATA_DIRS=${NEW_XDG_DATA_DIRS}" >> ~/.profile
+fi
+if ! grep -q "${NEW_XDG_DATA_DIRS}" ~/.zprofile ; then
+	echo "export XDG_DATA_DIRS=${NEW_XDG_DATA_DIRS}" >> ~/.zprofile
+fi
 
-echo "if [ -e $HOME/.nix-profile/etc/profile.d/nix.sh ]; then . $HOME/.nix-profile/etc/profile.d/nix.sh; fi" >> ~/.zprofile
+SRC_NIX="if [ -e ${HOME}/.nix-profile/etc/profile.d/nix.sh ]; then . ${HOME}/.nix-profile/etc/profile.d/nix.sh; fi"
+if ! grep -q "${SRC_NIX}" ~/.zprofile; then
+	echo "" >> ~/.zprofile
+fi
 
-echo "/home/diraol/.nix-profile/bin/zsh" | sudo tee -a /etc/shells > /dev/null
-sudo chsh -s /home/diraol/.nix-profile/bin/zsh
+NIX_ZSH="/home/diraol/.nix-profile/bin/zsh"
+if ! grep -q "${NIX_ZSH}" /etc/shells; then
+	echo "${NIX_ZSH}" | sudo tee -a /etc/shells > /dev/null
+fi
+sudo chsh -s "${NIX_ZSH}"
 
 # Natural scrolling
-echo "xinput set-prop 13 324 1" >> ~/.profile
-echo "xinput set-prop 13 324 1" >> ~/.zprofile
+NAT_SCRL="xinput set-prop 13 324 1"
+if ! grep -q "${NAT_SCRL}" ~/.profile; then
+	echo "${NAT_SCRL}" >> ~/.profile
+fi
+if ! grep -q "${NAT_SCRL}" ~/.zprofile; then
+	echo "${NAT_SCRL}" >> ~/.zprofile
+fi
